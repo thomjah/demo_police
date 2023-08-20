048beed2e594e7e46af81008fca00c9e06fc6264
Thomas Jahnsen <tjahnsen@ThomasMacBookPro2019.local>
2023-08-20 11:42:03 +0200

    Added summary block

--- src/app/components/prison-summary/prison-summary.component.ts
+++ src/app/components/prison-summary/prison-summary.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from "rxjs";
+
+import { DataService } from "src/app/services/data.service";
+import { Cell } from "src/app/types";
 
 @Component({
-  selector: 'app-prison-summary',
-  templateUrl: './prison-summary.component.html',
-  styleUrls: ['./prison-summary.component.scss']
+    selector: "app-prison-summary",
+    templateUrl: "./prison-summary.component.html",
+    styleUrls: ["./prison-summary.component.scss"],
 })
-export class PrisonSummaryComponent {
+export class PrisonSummaryComponent implements OnInit, OnDestroy {
+    private occupiedCells: Cell[] = [];
+    private dataSub: Subscription | undefined;
+
+    constructor(private dataService: DataService) {}
+
+    ngOnInit() {
+        this.dataSub = this.dataService.getCellGrid().subscribe((grid) => {
+            this.occupiedCells = grid.filter((cell) => cell.inmate !== null);
+        });
+    }
+
+    ngOnDestroy() {
+        this.dataSub?.unsubscribe();
+    }
+
+    getNumInmates(): number {
+        return this.occupiedCells.length;
+    }
+
+    getNumMale(): number {
+        return this.occupiedCells.filter(
+            (cell) => cell.inmate!.gender === "M"
+        ).length;
+    }
+
+    getNumFemale(): number {
+        return this.occupiedCells.filter(
+            (cell) => cell.inmate!.gender === "F"
+        ).length;
+    }
+
+    getNumReleasesWithinYear(): number {
+        const threshold = new Date();
+        threshold.setFullYear(threshold.getFullYear() + 1);
+        return this.occupiedCells.filter(
+            (cell) => cell.inmate!.endDate < threshold
+        ).length;
+    }
 
 }
